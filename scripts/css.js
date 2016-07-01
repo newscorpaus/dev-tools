eda29ac631ad0cc0ddf8c5a284430e8d4035d699
dionbeetson <dion.beetson@news.com.au>
2016-07-01 10:59:13 +1000

    Removed dummy  css path

--- scripts/css.js
+++ scripts/css.js
@@ -4,7 +4,7 @@ var utils = require('../build-tools/utils');
 var css = require('../build-tools/css');
 var path = require('path');
 
-utils.findFiles( ['./examples/*.scss', '/asdasdnasd/ss'], function(obj) {
+utils.findFiles( ['./examples/*.scss'], function(obj) {
 	var distFilename = path.basename(obj.file).replace('.scss', '.css');
 	var distDirectory = path.dirname(obj.file) + '/dist/';
 
